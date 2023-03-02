a9720cd81a105dffa3a1189ca65f40db7d29791e
Anderson Neves <andersonneves_@hotmail.com>
2023-03-02 16:59:13 -0300

    Criando um segundo objeto do tipo conta, Colocando atributos nos obejtos, Valores default dos atributos quando criamos os objetos

--- src/bytebank/CriaConta.java
+++ src/bytebank/CriaConta.java
@@ -3,10 +3,21 @@ package bytebank;
 public class CriaConta {
 	
 	public static void main(String[] args) {
-		// criando ou instanciando um objeto do tipo conta
-		Conta primeiraConta = new Conta();
+		Conta primeiraConta = new Conta();  // criando ou instanciando um objeto do tipo conta
 		primeiraConta.saldo = 200;
 		System.out.println("R$" + primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println("R$" + primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();  // criando um segundo objeto
+		segundaConta.saldo = 50;
+		
+		System.out.println("primeira conta tem R$" + primeiraConta.saldo);
+		System.out.println("segunda conta tem R$" + segundaConta.saldo);
+		
+		System.out.println(primeiraConta.agencia);  // ao criar um objeto todos seus atributos são zerados, o java faz isso
+		System.out.println(primeiraConta.numero);  // aqui e a linha anterior o resultado é zero
 	}
 
 }
