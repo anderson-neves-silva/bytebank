4a7c0a11d3177d687ecc1d814578151c97027320
Anderson Neves <andersonneves_@hotmail.com>
2023-03-03 16:05:07 -0300

    Criando o método saca que tem retorno

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -6,9 +6,16 @@ public class Conta {
 	int numero;
 	String titular;
 	
-	// criando um método ou comportamento, quando você não retorna nada como resposta em um método use o void
-	public void deposita(double valor) {
-		// saldo = saldo + valor; // a próxima linha faz o mesmo desta linha
+	public void deposita(double valor) {  // criando um método ou comportamento, quando você não retorna nada como resposta em um método use o void
 		this.saldo += valor;  // o this ele se refere ao objeto criando onde usamos o new, ele se refere a esse objeto
+	}
+	
+	public boolean saca(double valor) {  // criando um método com retorno que faz o saque, aqui a resposta é devolvida como um boolean se é possível sacar ou não
+		if (this.saldo >= valor) {  // esse if verifica se a conta possui dinheiro suficiente para o saque
+			this.saldo -= valor;
+			return true;  // o return para a execução do método e volta para quem estava chamando e retorna o valor
+		} else {
+			return false;
+		}
 	}
 }
--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -7,6 +7,10 @@ public class TestaMetodo {
 		contaDoAnderson.saldo = 100;  // adicionando 100 ao saldo
 		contaDoAnderson.deposita(55);  // invocando o método deposita do objeto conta, e depositando 55 na conta
 		System.out.println(contaDoAnderson.saldo);
+		
+		boolean conseguiuRetirar = contaDoAnderson.saca(20);  // invocando o método saca que possui retorno, sacando 20, guardando o retorno do método em uma variável
+		System.out.println("novo saldo: R$" + contaDoAnderson.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 
 }
